9c392f14ad7c2227335006b4b15eb96609737089
ayvc0420 <ayvc0420@gmail.com>
2021-09-05 21:12:49 +0800

    0905_mediaQuery
    詳細點選說明.txt

---
+++ 練習/mediaQuery/0905/js/work.js
@@ -0,0 +1,48 @@
+$(function(){
+
+    $('#nva_menu_2').hover(function(){
+        $('#nav_show').stop().toggle(500)
+    },function(){
+        $('#nav_show').stop().toggle(500);
+    })
+
+    $('#about_menu_show , #about_menu').hover(function(){
+        $('#about_menu').stop().toggle()
+    },function(){
+        $('#about_menu').stop().toggle()
+    })
+
+    $('.item1').hover(function(){
+        $('.item1 div img').stop().addClass("rotate_animation");
+    },(function(){
+        $('.item1 div img').stop().removeClass("rotate_animation");
+    }))
+    $('.item2').hover(function(){
+        $('.item2 div img').stop().addClass("rotate_animation");
+    },(function(){
+        $('.item2 div img').stop().removeClass("rotate_animation");
+    }))
+    $('.item3').hover(function(){
+        $('.item3 div img').stop().addClass("rotate_animation");
+    },(function(){
+        $('.item3 div img').stop().removeClass("rotate_animation");
+    }))
+    $('.item4').hover(function(){
+        $('.item4 div img').stop().addClass("rotate_animation");
+    },(function(){
+        $('.item4 div img').stop().removeClass("rotate_animation");
+    }))
+    $('.item5').hover(function(){
+        $('.item5 div img').stop().addClass("rotate_animation");
+    },(function(){
+        $('.item5 div img').stop().removeClass("rotate_animation");
+    }))
+    $('.item6').hover(function(){
+        $('.item6 div img').stop().addClass("rotate_animation");
+    },(function(){
+        $('.item6 div img').stop().removeClass("rotate_animation");
+    }))
+
+
+
+})
