cae3964379e0ffb1738ca5d31ffe90ceacaddb8b
ayvc0420 <ayvc0420@gmail.com>
2021-09-11 23:30:19 +0800

    0911_exercise

---
+++ 練習/mediaQuery/0911/js/javascript.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $('.pc_nav_2').hover(function(){
+        $('#pc_nav_div').stop().toggle(200)
+    },function(){
+        $('#pc_nav_div').stop().toggle(200)
+    })
+
+
+    // $('.pc_nav_2').hover(function(){
+    //     $('.pc_nav_2 div').stop().css({'display':'block'});
+    // },function(){
+    //     $('.pc_nav_2 div').stop().css({'display':'none'});
+    // })
+
+
+})
