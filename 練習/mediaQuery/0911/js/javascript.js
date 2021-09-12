5aae92c28485815a3b3c56dc725e321a8816ba5e
ayvc0420 <ayvc0420@gmail.com>
2021-09-12 15:34:17 +0800

    css_width

--- 練習/mediaQuery/0911/js/javascript.js
+++ 練習/mediaQuery/0911/js/javascript.js
@@ -2,16 +2,13 @@ $(function(){
 
     $('.pc_nav_2').hover(function(){
         $('#pc_nav_div').stop().toggle(200)
+        $('.pc_nav_2 img').css({'opacity':'1'})
     },function(){
         $('#pc_nav_div').stop().toggle(200)
+        $('.pc_nav_2 img').css({'opacity':'0.5'})
     })
 
-
-    // $('.pc_nav_2').hover(function(){
-    //     $('.pc_nav_2 div').stop().css({'display':'block'});
-    // },function(){
-    //     $('.pc_nav_2 div').stop().css({'display':'none'});
-    // })
+    
 
 
 })
