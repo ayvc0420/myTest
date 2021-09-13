1ca4494740879a577570e668e70891db1159b085
ayvc0420 <ayvc0420@gmail.com>
2021-09-13 23:12:04 +0800

    pc_ok_width1200px

--- 練習/mediaQuery/0911/js/javascript.js
+++ 練習/mediaQuery/0911/js/javascript.js
@@ -8,7 +8,9 @@ $(function(){
         $('.pc_nav_2 img').css({'opacity':'0.5'})
     })
 
-    
-
+    const buy_btn = document.getElementById('buy_btn_id');
+    buy_btn.addEventListener('click', function (){
+        alert('你不會真的想買吧？參照網站再說明那邊喔~~。')
+    })
 
 })
