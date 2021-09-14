0f00ec67ac159a8d6314a10810a62e074a1a5f87
ayvc0420 <ayvc0420@gmail.com>
2021-09-14 21:54:09 +0800

    wrap重寫

--- 練習/mediaQuery/0911/js/javascript.js
+++ 練習/mediaQuery/0911/js/javascript.js
@@ -13,4 +13,19 @@ $(function(){
         alert('你不會真的想買吧？參照網站再說明那邊喔~~。')
     })
 
+    $('#mob_menu_btn').click(function(){
+        $('.mob_menu').toggle()
+        $('.pc_nav , .logo , .wrap , .main , .slider ,.footer_area_bg , footer').toggle()
+    })
+    $('#mob_close_btn').click(function(){
+        $('.mob_menu').toggle()
+        $('.pc_nav , .logo , .wrap , .main , .slider ,.footer_area_bg , footer').toggle()
+    })
+
+    $('.mob_li_btn').click(function(){
+        $('.mob_container ul>li:nth-of-type(2) li').toggle()
+        $('.mob_li_btn img:nth-of-type(1)').toggle()
+        $('.mob_li_btn img:nth-of-type(2)').toggle()
+    })
+
 })
