260fd30ea7caa443debf6ecabf2c07d0b27d8b05
ayvc0420 <ayvc0420@gmail.com>
2021-11-11 00:35:14 +0800

    1110

---
+++ 練習/每日練習/1110/vuecli/src/components/InputTest/index.vue
@@ -0,0 +1,26 @@
+<style src="./style.css" scoped></style>
+<template src="./template.html"></template>
+
+
+<script>
+export default {
+    name:'input-test',
+    props:{
+        text:{
+            type:String,
+            required:true
+        }
+    },
+    computed:{
+        inputText:{
+            get(){
+                return this.text
+            },
+            set(val){
+                this.$emit('update:text',val)
+            }
+        }
+    }
+
+}
+</script>
---
+++ 練習/每日練習/1110/vuecli/src/components/Title.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1 class="title_h1">{{title}}</h1>
+
+</template>
+
+<style scoped>
+    .title_h1{
+        font-size: 36px;
+        color: cadetblue;
+    }
+</style>
+
+
+<script>
+export default {
+    name:'TitleTest',
+    // data(){
+    //     return{
+    //         title:'this is title test. - 2021/11/10'
+    //     }
+    // }
+    props:{
+        title:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
